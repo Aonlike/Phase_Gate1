783696f25d184bb95358df798f6870f60f8f79ef
Martin Ngutswen <martinngutswen02@gmail.com>
2025-08-21 17:09:50 +0100

    done

---
+++ JavaScript/Day4/GroceryApp .js
@@ -0,0 +1,41 @@
+let GroceryApp = [];
+
+function showMenu() {
+  return prompt(
+    "Grocery Menu:" 
+    "1. Add Item" +
+    "2. Remove Item"
+    "3. Show Items"
+    "4. Exit"
+    "Enter your choice (1-4):"
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Python/Day4/grocery_app.py
@@ -0,0 +1,16 @@
+def show_menu():
+    	print("Enter an option");
+	print("1. Add an item:");
+	print("2. Remove an item:");
+	print("3. Availabe items:");
+	print("4. Exit.");
+
+
+
+
+
+
+
+
+
+
