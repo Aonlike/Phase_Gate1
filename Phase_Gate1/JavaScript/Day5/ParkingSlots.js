8f84d0728ddccc7dbd8f15ba5f606d5dae4c8168
Martin Ngutswen <martinngutswen02@gmail.com>
2025-08-22 17:03:49 +0100

    First

---
+++ Phase_Gate1/JavaScript/Day5/ParkingSlots.js
@@ -0,0 +1,29 @@
+//1. Create a parking space that will accept 20 cars
+//2. store each of the cars entered
+//3. The slot should be occupied or it should be empty
+//4. A car can leave a slot
+//5. Display an entry message if a car enters a slot
+//6. Display an exit message if a car leaves a slot
+
+
+let ParkingSlots = {0,0,0,1,1,1,1,0,1,0,1,0,1,1,1,1,0,0,1,1}
+
+function enterPark(){
+    for(int slot = 0; slot < parkingSlots.length; slot++){
+        if (let parkingSlots[slot] == 0)
+            parking_slots[slot] = 1
+            console.log("Car entered at slot")
+            return}
+  console.log("No parking space!")
+
+
+function leavePark(){
+let index = slot_number - 1
+    if 0 <= index < len(parking_lot):
+        if parking_lot[index] == 1:
+            parking_lot[index] = 0
+            console.log(Car left from slot")
+        else:
+            console.log("Slot is already empty.")
+ 
+
