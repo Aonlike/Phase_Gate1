9d1cf90d881037c4f83d86dd8de9ccc3979760ba
Martin Ngutswen <martinngutswen02@gmail.com>
2025-08-22 16:55:54 +0100

    Day5

---
+++ Phase_Gate1/Java/Day5/MiniParkingSystem.java
@@ -0,0 +1,43 @@
+//1. Create a parking space that will accept 20 cars
+//2. store each of the cars entered
+//3. The slot should be occupied or it should be empty
+//4. A car can leave a slot
+//5. Display an entry message if a car enters a slot
+//6. Display an exit message if a car leaves a slot
+
+
+public class MiniParkingSystem{
+	public static void main(String[] args){
+	
+int [] parkingSlots = {0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1};
+
+	for(int slot = 0; slot < parkingSlots.length; slot++)
+		if(parking_slots[slot] == 0){system.out.print("You can park");
+		}
+		else if(parking_slots[slot] == 1){system.out.print("No space");}
+	else{System.out.print(parkingSlots[slot] + " ");}
+
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
